59b969f8d007a1895c6e6d31562631c07f7b9469
akkorismeglesz <124844139+akkorismeglesz@users.noreply.github.com>
2023-04-09 15:25:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const require = Node.js
+
+/*document.addEventListener('DOMContentLoaded', () => {const quoteText = document.getElementById('quote-text'),
+quoteTags = document.getElementById('quote-tags'),
+quoteAuthor = document.getElementById('quote-author'),
+getNewQuote = document.getElementById('getNewQuote');
+
+fetch("https://api.quotable.io")
+.then(Response => Response.json())
+.then(data => {
+   quoteText.textContent = data.content;
+   quoteTags.textContent = data.tags;
+   quoteAuthor.textContent = `${data.author}`;
+})});*/
+
+/*function getQoute() {
+   fetch("https://api.quotable.io/random")
+   .then(Response => Response.json())
+   .then(data => {
+      const quoteText = data.content;
+      const quoteTags = data.tags;
+      const quoteAuthor = data.author;
+      console.log(`${quoteText} - ${quoteTags} - ${quoteAuthor}`);
+   })
+   .catch(error => {
+      console.log("An error occured while fetching the quote:",error);
+   })
+}
+
+getQoute();*/
+
+/*function getQuote() {
+   fetch("https://api.quotable.io/random")
+     .then(response => response.json())
+     .then(data => {
+       const quoteText = data.content;
+       const quoteAuthor = data.author;
+       console.log(`${quoteText} - ${quoteAuthor}`);
+     })
+     .catch(error => {
+       console.log("An error occurred while fetching the quote:", error);
+     });
+ }
+ 
+ getQuote();*/
+
+const quoteText = document.getElementById('quote-text'),
+      quoteTags = document.getElementById('quote-tags'),
+      quoteAuthor = document.getElementById('quote-author'),
+      getNewQuote = document.getElementById('getnewquote');
+
+function randomQuote(){
+fetch("https://api.quotable.io")
+.then(response => response.json())
+.then(data => {
+   quoteText.textContent = data.content;
+   quoteTags.textContent = data.tags;
+   quoteAuthor.textContent = `${data.author}`;
+})
+.catch(error => {
+   console.log("An error occured while fetching the quote",error);
+})
+}
+
+randomQuote();
+getNewQuote.addEventListener('click', () => {randomQuote();});
